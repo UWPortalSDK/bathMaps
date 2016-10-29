961cb4c90a1410d130d3993de59218f64b1d2118
Colin Marsch <colinmarsch@gmail.com>
2016-10-29 17:08:41 -0400

    Updated user branch from Portal workspace (server script)

--- serverscript.js
+++ serverscript.js
@@ -20,12 +20,14 @@ function addRating() {
  	if (score > 5 || score < 1) {
    		return '{"result":"error"}';  	   
     } else {
-     	db.Execute('INSERT INTO ratings VALUES (washroomName, score)');
+     	db.Execute('INSERT INTO ratings VALUES (@name, @value)');
     }
+    
+    return getAvg();
 }
 
 function getAvg() {
-	var washroomName = args.Get("name");
-    var average = db.Execute('SELECT washroom, AVG(rating) FROM ratings WHERE washroom = washroomName GROUP BY washroom').column1;
-    return average;
+    var raw = db.Execute('SELECT AVG(rating) FROM ratings WHERE washroom = @name GROUP BY washroom');
+    var average = JSON.parse(raw);
+    return average[0].Column1;
 }
